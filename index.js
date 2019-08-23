b631b7566f1186928bf3840eb5234f4592bee153
Tiago Monteiro <tiagoasmont@gmail.com>
2019-08-23 18:06:56 +0100

    socket.io to receive updates from the master/market todo: send jobs to volunteers

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const amqp = require('amqplib');
+const io = require('socket.io-client');
+const socket = io('http://localhost:8081');
+
+const request = require('request');
+
+
+let volunteers = {};
+
+socket.on('connect', function () {
+    console.log("connected")
+});
+
+socket.on('event', function (data) {
+    console.log(data)
+});
+
+
+socket.on('update-volunteers', (data) => {
+    console.log(data)
+    volunteers = data;
+});
+
+
+socket.on('disconnect', function () {
+    console.log("disconnected")
+});
 
 
 async function init() {
@@ -15,12 +41,25 @@ async function init() {
     });
 
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-    channel.consume(queue, function(msg) {
+    channel.consume(queue, function (msg) {
         console.log(" [x] Received %s", msg.content.toString());
     }, {
-        noAck: true
+        noAck: false
     });
 
 }
 
 init();
+
+
+setInterval(function () {
+    for (let key in volunteers) {
+        let volunteer = volunteers[key];
+        request('http://' + volunteer.ip + ':' + volunteer.port + '/healthz', function (error, response, body) {
+            console.error('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
+        });
+    }
+
+}, 5000);
